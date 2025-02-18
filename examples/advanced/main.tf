7e925d38df61b544d4f154a745367d6926584013
Aashiq-J <122446118+Aashiq-J@users.noreply.github.com>
2025-02-18 09:20:36 +0530

    Update main.tf

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -75,7 +75,7 @@ module "private_path" {
   subnet_id                          = ibm_is_subnet.provider_subnet.id
   nlb_name                           = "${var.prefix}-nlb"
   private_path_name                  = "${var.prefix}-pp"
-  private_path_service_endpoints     = ["private-path-example.ibm.com"]
+  private_path_service_endpoints     = ["vpc-pps.example.com"]
   private_path_default_access_policy = "permit"
   nlb_pool_member_instance_ids       = [for vsi in ibm_is_instance.vsi : vsi.id]
   nlb_pool_member_port               = 80
