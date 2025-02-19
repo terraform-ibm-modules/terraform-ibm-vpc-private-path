1f164dfe8f535f054be0fd7bbf811a5eb304e233
Aashiq-J <122446118+Aashiq-J@users.noreply.github.com>
2025-02-19 11:12:42 +0530

    Update variables.tf

--- examples/advanced/variables.tf
+++ examples/advanced/variables.tf
@@ -2,14 +2,6 @@
 # Input variables
 ########################################################################################################################
 
-#
-# Module developer tips:
-#   - Examples are references that consumers can use to see how the module can be consumed. They are not designed to be
-#     flexible re-usable solutions for general consumption, so do not expose any more variables here and instead hard
-#     code things in the example main.tf with code comments explaining the different configurations.
-#   - For the same reason as above, do not add default values to the example inputs.
-#
-
 variable "ibmcloud_api_key" {
   type        = string
   description = "The IBM Cloud API Key."
