579be8ffb49395139c6704554d1a153a230861ac
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-02-20 20:38:50 +0530

    precommit

--- examples/advanced/userdata.sh
+++ examples/advanced/userdata.sh
@@ -2,7 +2,7 @@
 yum install -y bind-utils nginx
 
 rm -f /usr/share/nginx/html/index.html
-echo "Hello world from `hostname`" > /usr/share/nginx/html/index.html
+echo "Hello world from $(hostname)" > /usr/share/nginx/html/index.html
 chmod go+r /usr/share/nginx/html/index.html
 
 systemctl start nginx
