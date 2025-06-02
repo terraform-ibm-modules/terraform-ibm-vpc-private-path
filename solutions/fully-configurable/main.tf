abc92123dcd031fd2db479694eb2cb1e67d15652
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-06-02 10:58:22 +0530

    update

--- solutions/fully-configurable/main.tf
+++ solutions/fully-configurable/main.tf
@@ -14,7 +14,7 @@ locals {
   private_path_name         = "${local.prefix}${var.private_path_name}"
   subnet_id                 = var.existing_subnet_id != null ? var.existing_subnet_id : data.ibm_is_vpc.vpc[0].subnets[0].id
   # tflint-ignore: terraform_unused_declarations
-  validate_number_of_subnets = var.existing_vpc_id == null && length(data.ibm_is_vpc.vpc[0].subnets) > 0 ? true : tobool("Existing VPC should have atleast one subnet.")
+  validate_number_of_subnets = var.existing_vpc_id != null ? length(data.ibm_is_vpc.vpc[0].subnets) > 0 ? true : tobool("Existing VPC should have atleast one subnet.") : true
 }
 
 data "ibm_is_vpc" "vpc" {
--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -33,7 +33,7 @@ variable "region" {
 variable "prefix" {
   type        = string
   nullable    = true
-  description = "The prefix to be added to all resources created by this solution. To skip using a prefix, set this value to null or an empty string. The prefix must begin with a lowercase letter and may contain only lowercase letters, digits, and hyphens '-'. It should not exceed 16 characters, must not end with a hyphen('-'), and can not contain consecutive hyphens ('--'). Example: `prod-0205-cos`."
+  description = "The prefix to be added to all resources created by this solution. To skip using a prefix, set this value to null or an empty string. The prefix must begin with a lowercase letter and may contain only lowercase letters, digits, and hyphens '-'. It should not exceed 16 characters, must not end with a hyphen('-'), and can not contain consecutive hyphens ('--'). Example: `prod-0205-private-path`. [Learn more](https://terraform-ibm-modules.github.io/documentation/#/prefix.md)"
 
   validation {
     condition = (var.prefix == null || var.prefix == "" ? true :
