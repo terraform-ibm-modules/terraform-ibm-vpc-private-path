7eec6eb8221cf1a0aa5c50a56e0b277eb3dd1ce1
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-08-16 12:50:59 +0530

    chore(deps): update dependencies (#82)

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -122,7 +122,7 @@ resource "ibm_is_subnet" "consumer_subnet" {
 
 module "vpe" {
   source            = "terraform-ibm-modules/vpe-gateway/ibm"
-  version           = "4.7.4"
+  version           = "4.7.5"
   resource_group_id = module.resource_group.resource_group_id
   vpc_id            = ibm_is_vpc.consumer_vpc.id
   cloud_service_by_crn = [
