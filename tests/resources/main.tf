df2e64c7969bca5b979536488d227ea2ab21f61b
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-12-13 19:23:10 +0000

    fix(deps): update dependencies

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -3,7 +3,7 @@
 ################################################################################
 module "resource_group" {
   source              = "terraform-ibm-modules/resource-group/ibm"
-  version             = "1.4.4"
+  version             = "1.4.6"
   resource_group_name = "${var.prefix}-rg"
 }
 
