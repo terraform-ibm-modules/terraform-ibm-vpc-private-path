795b45046a2f1fdb904c3a8b3dbdc72efac2fb4a
Aashiq Jacob <122446118+Aashiq-J@users.noreply.github.com>
2025-12-31 18:33:16 +0530

    feat: updated the DA dependencies (#125)

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -3,16 +3,21 @@ package test
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"strings"
 	"testing"
 
+	"github.com/IBM/go-sdk-core/v5/core"
 	"github.com/gruntwork-io/terratest/modules/files"
 	"github.com/gruntwork-io/terratest/modules/logger"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/cloudinfo"
+	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/common"
+	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testaddons"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testschematic"
 )
@@ -23,6 +28,28 @@ const resourceGroup = "geretain-test-resources"
 // Ensure every example directory has a corresponding test
 const advancedExampleDir = "examples/advanced"
 const fullyConfigurableTerraformDir = "solutions/fully-configurable"
+
+// Define a struct with fields that match the structure of the YAML data
+const yamlLocation = "../common-dev-assets/common-go-assets/common-permanent-resources.yaml"
+
+var (
+	sharedInfoSvc      *cloudinfo.CloudInfoService
+	permanentResources map[string]interface{}
+)
+
+// TestMain will be run before any parallel tests, used to set up a shared InfoService object to track region usage
+// for multiple tests
+func TestMain(m *testing.M) {
+	sharedInfoSvc, _ = cloudinfo.NewCloudInfoServiceFromEnv("TF_VAR_ibmcloud_api_key", cloudinfo.CloudInfoServiceOptions{})
+
+	var err error
+	permanentResources, err = common.LoadMapFromYaml(yamlLocation)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	os.Exit(m.Run())
+}
 
 func setupOptions(t *testing.T, prefix string, dir string) *testhelper.TestOptions {
 	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
@@ -208,4 +235,67 @@ func TestRunUpgradeFullyConfigurableInSchematics(t *testing.T) {
 		terraform.WorkspaceDelete(t, existingTerraformOptions, prefix)
 		logger.Log(t, "END: Destroy (existing resources)")
 	}
+}
+
+func TestAddonsDefaultConfiguration(t *testing.T) {
+	t.Parallel()
+
+	// Verify ibmcloud_api_key variable is set
+	checkVariable := "TF_VAR_ibmcloud_api_key"
+	val, present := os.LookupEnv(checkVariable)
+	require.True(t, present, checkVariable+" environment variable not set")
+	require.NotEqual(t, "", val, checkVariable+" environment variable is empty")
+
+	// Programmatically determine region to use based on availability
+	region, _ := testhelper.GetBestVpcRegion(val, "../common-dev-assets/common-go-assets/cloudinfo-region-vpc-gen2-prefs.yaml", "eu-de")
+
+	options := testaddons.TestAddonsOptionsDefault(&testaddons.TestAddonOptions{
+		Testing:   t,
+		Prefix:    "vpc-pps",
+		QuietMode: false, // Suppress logs except on failure
+	})
+
+	options.AddonConfig = cloudinfo.NewAddonConfigTerraform(
+		options.Prefix,
+		"deploy-arch-ibm-is-private-path-service-gateway",
+		"fully-configurable",
+		map[string]interface{}{
+			"region":                         region,
+			"private_path_service_endpoints": []string{"vpc-pps.dev.internal"},
+		},
+	)
+
+	options.AddonConfig.Dependencies = []cloudinfo.AddonConfig{
+		// The VPC must be created explicitly because it is disabled by default in the catalog configuration due to this issue(https://github.ibm.com/ibmcloud/content-catalog/issues/6057).
+		{
+			OfferingName:   "deploy-arch-ibm-slz-vpc",
+			OfferingFlavor: "fully-configurable",
+			Enabled:        core.BoolPtr(true),
+			Inputs: map[string]interface{}{
+				"region": region,
+			},
+			Dependencies: []cloudinfo.AddonConfig{
+				// Disable target / route creation to prevent hitting quota in account
+				{
+					OfferingName:   "deploy-arch-ibm-cloud-monitoring",
+					OfferingFlavor: "fully-configurable",
+					Inputs: map[string]interface{}{
+						"enable_metrics_routing_to_cloud_monitoring": false,
+					},
+					Enabled: core.BoolPtr(true),
+				},
+				{
+					OfferingName:   "deploy-arch-ibm-activity-tracker",
+					OfferingFlavor: "fully-configurable",
+					Inputs: map[string]interface{}{
+						"enable_activity_tracker_event_routing_to_cloud_logs": false,
+					},
+					Enabled: core.BoolPtr(true),
+				},
+			},
+		},
+	}
+
+	err := options.RunAddonTest()
+	require.NoError(t, err)
 }
