b83978c9491329021f75d21d047552ab1b8a6a17
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-04-04 08:27:53 +0000

    fix(deps): update dependencies

--- solutions/fully-configurable/main.tf
+++ solutions/fully-configurable/main.tf
@@ -21,7 +21,7 @@ locals {
 
 module "existing_vpc_crn_parser" {
   source  = "terraform-ibm-modules/common-utilities/ibm//modules/crn-parser"
-  version = "1.4.2"
+  version = "1.4.3"
   crn     = var.existing_vpc_crn
 }
 
