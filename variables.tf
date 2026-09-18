c07de5f74d271e546726301e308538da4e5fca81
Harika <86517031+HarikaPonna@users.noreply.github.com>
2026-09-18 18:09:24 +0530

    fix: Add `tags` and `access_tags` parameters to `ibm_is_private_path_service_gateway` (#197)

--- variables.tf
+++ variables.tf
@@ -8,8 +8,9 @@ variable "resource_group_id" {
 
 variable "resource_tags" {
   type        = list(string)
-  description = "Add user resource tags to the Private Path service to organize, track, and manage costs. [Learn more](https://cloud.ibm.com/docs/account?topic=account-tag&interface=ui#tag-types)."
+  description = "Add user resource tags to the Private Path service and Load Balancer to organize, track, and manage costs. [Learn more](https://cloud.ibm.com/docs/account?topic=account-tag&interface=ui#tag-types)."
   default     = []
+  nullable    = false
   validation {
     condition     = alltrue([for tag in var.resource_tags : can(regex("^[A-Za-z0-9 _\\-.:]{1,128}$", tag))])
     error_message = "Each resource tag must be 128 characters or less and may contain only A-Z, a-z, 0-9, spaces, underscore (_), hyphen (-), period (.), and colon (:)."
@@ -18,8 +19,9 @@ variable "resource_tags" {
 
 variable "access_tags" {
   type        = list(string)
-  description = "Add access management tags to the Private Path service to control access. [Learn more](https://cloud.ibm.com/docs/account?topic=account-tag&interface=ui#create-access-console)."
+  description = "Add access management tags to the Private Path service and Load Balancer to control access. [Learn more](https://cloud.ibm.com/docs/account?topic=account-tag&interface=ui#create-access-console)."
   default     = []
+  nullable    = false
 
   validation {
     condition = alltrue([
